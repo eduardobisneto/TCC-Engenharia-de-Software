18b2d2d670b555f00b74a4b947aa5ab757627679
Eduardo Vieira <eduardovieira2@gmail.com>
2018-10-04 20:46:47 -0300

    Enviando arquivos da Entity

---
+++ API/API.Common.Entity/Core/Base.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace API.Common.Entity.Core
+{
+    public class Base
+    {
+        public int Id { get; set; }
+
+        private DateTime? _DataCriacao { get; set; }
+        public DateTime? DataCriacao
+        {
+            get { return _DataCriacao.HasValue ? _DataCriacao.Value : DateTime.Now; }
+            set { _DataCriacao = value; }
+        }
+
+        public DateTime? DataAlteracao { get; set; }
+    }
+    
+}
---
+++ API/API.Common.Entity/Core/Pessoa.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Common.Entity.Core
+{
+    public class Pessoa : Base
+    {
+        public int IdTipoPessoa { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
